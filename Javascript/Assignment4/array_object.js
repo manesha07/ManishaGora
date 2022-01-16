f3cd1a9640e78fa3c40ef138aba93d6db7735142
manesha07 <manishagora52@gmail.com>
2022-01-16 22:11:20 +0545

    js assignment

--- Javascript/Assignment4/array_object.js
+++ Javascript/Assignment4/array_object.js
@@ -15,7 +15,7 @@
 // Kritika Baral
 // Srijana Thulung
 
-// Convert the list of names in array of object like:
+// Q1) Convert the list of names in array of object like:
 // [
 //     {
 //       id: 1,
@@ -44,15 +44,14 @@
 //   console.log(final);
 // }
 
-
 var names = ['Neeta sapkota','neha shiwakoti','Srijana Khatiwada','Smrity Dhakal','Sami Chakradhar','Kirtee Maharjan','Trija Thebe','Sindhu Aryal','Kusum Ranjitkar','Elisha Bista','Rachana kafle','Barsha Maharjan','Pooja Gurung','Bisikha Subedi','Kritika Baral','Srijana Thulung'];
 var obj = {};
-final=[];
+var final=[];
 for (let i = 0; i < names.length; i++) {
   var splitted=names[i].split(" ");
-    obj.id = i;
+    obj.id = i+1;
     obj.firstname=splitted[0];
-    obj.lastname=splitted[1]
+    obj.lastname=splitted[1];
     final[i]=obj;
     obj={};
 }
@@ -60,35 +59,49 @@ for (let i = 0; i < names.length; i++) {
 // print object
 // console.log(final);
 
-final_result=[]
+var final_result=[];
 names.forEach((element,index) => {
   var splitted=names[index].split(" ");
   obj.id = index;
   obj.firstname=splitted[0];
-  obj.lastname=splitted[1]
+  obj.lastname=splitted[1];
   final_result[index]=obj;
   obj={};
 });
-// console.log(final_result);
+ console.log(final_result);
 
-
+// Question 2
+// Given the result of the above problem, create a function which takes a character and prints the count of first names which starts with letter that letter and which donot start with given letter.  
 function find(variable) {
   Countfirstletter = final_result.filter(function(value){
-    return value.firstname[0]==variable;
+    return value.firstname[0] == variable.toUpperCase();
   });
   counter=Countfirstletter.length;
   console.log("Started with '" + variable + "' :" + counter);
   console.log("Didn't Started with '" + variable + "' :" + (final_result.length-counter));
 }
-find("S");
+find("s");
+find("a");
 
+// Question 3
+// Convert the array of the result in Question 1, in following format
+// {
+//   1: {
+//     firstName: 'Neeta',
+//     lastName: 'Sapkota'
+//   },
+//   2: {
+//     firstName: 'Neha',
+//     lastName: 'Shiwakoti'
+//   },
+//   ...
+// }
 
-new_object={};
-object={};
+var new_object={};
+var object={};
 final_result.forEach((element,index) => {
-  object=element.slice();
-  console.log(object);
-  // new_object[index+1]=object;
-  // object={};
+  object=element;
+  delete object['id'];
+  new_object[index+1] = object;
 });
 console.log(new_object);
