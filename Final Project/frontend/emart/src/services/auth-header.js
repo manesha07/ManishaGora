1cc7a484a5982b02fc23dccd8a3b30e084c9e99b
manesha07 <manishagora52@gmail.com>
2024-02-25 14:48:14 +1100

    removed console

--- Final Project/frontend/emart/src/services/auth-header.js
+++ Final Project/frontend/emart/src/services/auth-header.js
@@ -1,10 +1,8 @@
 
 export default function authHeader() {
     const user = JSON.parse(localStorage.getItem("user"));
-    console.log("uutmathiokeb",user);
     if (user && user.token) {
       return { Authorization: `Bearer ${ user.token}` };
-    //   return { "x-auth-token": user.accessToken };
     } else {
       return {};
     }
--- Final Project/frontend/emart/src/services/product.js
+++ Final Project/frontend/emart/src/services/product.js
@@ -10,7 +10,6 @@ const getCurrentUser = () => {
 export const fetchProducts = async (query) => {
   const url = `${config.apiUrl}${config.endpoints.products}`;
   const data = await axios.get(url);
-  console.log("data",data);
   return data.data.data;
 };
 
@@ -22,15 +21,12 @@ export const fetchProductsById = async (id) => {
 
 export const AddProduct= async(addData) => {
   try {
-    console.log("inside",addData);
     const url = `${config.apiUrl}${config.endpoints.products}`;
     const data = await axios.post(url,addData, { headers: authHeader() }
     );
-    console.log("Added data",data.data.data);
     return data.data.data;
   }
    catch (err) {
-    console.log("yehi err",err);
     return err.response.data;
   }
 };
@@ -40,11 +36,9 @@ export const EditProduct= async(EditData,id) => {
     const url = `${config.apiUrl}${config.endpoints.product}`;
     const data = await axios.put(interpolate(url, { id }), EditData, { headers: authHeader() }
     );
-    console.log("edited data",data.data.data);
     return data.data.data;
   }
    catch (err) {
-     console.log("yehi details",err.response.data.details);
     return err.response.data;
   }
 };
@@ -53,10 +47,9 @@ export const deleteProduct = async (id) => {
   try{
   const url = `${config.apiUrl}${config.endpoints.product}`;
   const data = await axios.delete(interpolate(url, { id }), { headers: authHeader() });
-  console.log("deleted data",data);
+
   return data.data.data;}
   catch (err) {
-    console.log("yehi err",err);
    return err.response.data;
  }
 };
